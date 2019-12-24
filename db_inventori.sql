638354c7ec2b89b69956e4a0f2632fc19be430ae
arifrusman <arif83.ar@gmail.com>
2019-12-25 00:39:21 +0700

    update db_inventori.sql

--- db_inventori.sql
+++ db_inventori.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2019 at 04:00 PM
--- Server version: 10.3.16-MariaDB
--- PHP Version: 7.1.30
+-- Generation Time: Dec 24, 2019 at 05:51 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.1.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,7 +31,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `agamas` (
   `id_agama` int(10) UNSIGNED NOT NULL,
   `nama_agama` varchar(20) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -53,9 +53,9 @@ INSERT INTO `agamas` (`id_agama`, `nama_agama`, `created_at`, `updated_at`) VALU
 --
 
 CREATE TABLE `categories` (
-  `id_kategori` int(10) NOT NULL,
+  `id_kategori` int(10) UNSIGNED NOT NULL,
   `nama_kategori` varchar(100) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -84,7 +84,7 @@ CREATE TABLE `employees` (
   `id_agama` int(10) UNSIGNED DEFAULT NULL,
   `alamat` varchar(255) DEFAULT NULL,
   `telp` varchar(20) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -107,7 +107,7 @@ INSERT INTO `employees` (`id_karyawan`, `sap`, `nama_karyawan`, `id_gender`, `tg
 CREATE TABLE `genders` (
   `id_gender` int(10) UNSIGNED NOT NULL,
   `nama_gender` varchar(100) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -165,26 +165,115 @@ INSERT INTO `password_resets` (`email`, `token`, `created_at`) VALUES
 --
 
 CREATE TABLE `products` (
-  `id_produk` int(10) NOT NULL,
+  `id_produk` int(10) UNSIGNED NOT NULL,
   `kode_produk` varchar(100) NOT NULL,
   `nama_produk` varchar(100) NOT NULL,
-  `id_kategori` int(10) NOT NULL,
+  `id_kategori` int(10) UNSIGNED NOT NULL,
   `image` varchar(100) NOT NULL,
   `stok_produk` int(11) NOT NULL,
-  `id_unit` int(10) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `id_unit` int(10) UNSIGNED NOT NULL,
+  `id_supplier` int(10) UNSIGNED NOT NULL,
+  `lokasi` varchar(200) NOT NULL,
+  `ket_produk` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id_produk`, `kode_produk`, `nama_produk`, `id_kategori`, `image`, `stok_produk`, `id_unit`, `created_at`, `updated_at`) VALUES
-(3, 'MM001', 'Steel hub', 2, '1574913140.jpg', 20, 4, '2019-11-28 03:52:22', '2019-11-28 03:52:22'),
-(5, 'MB001', 'Franklin dumb', 2, '1574941221.jpg', 20, 3, '2019-11-28 05:01:54', '2019-11-28 11:40:21'),
-(6, 'GB0009', 'hoosing dump', 2, '1574925099.jpg', 20, 4, '2019-11-28 07:11:39', '2019-11-28 14:59:54'),
-(7, 'GB0005', 'demian slam', 4, '1574953069.jpg', 20, 4, '2019-11-28 14:57:49', '2019-11-28 14:57:49');
+INSERT INTO `products` (`id_produk`, `kode_produk`, `nama_produk`, `id_kategori`, `image`, `stok_produk`, `id_unit`, `id_supplier`, `lokasi`, `ket_produk`, `created_at`, `updated_at`) VALUES
+(1, 'MH009', 'Metal Ring', 1, '1577107585.jpg', 30, 3, 1, 'rak 4, kolom12', 'Flamable', '2019-12-23 13:26:25', '2019-12-23 13:26:25'),
+(2, 'MB0041', 'Demian Hub', 2, '1577107635.jpg', 28, 4, 3, 'rak 4, kolom 23', 'Mudah pecah', '2019-12-23 13:27:15', '2019-12-23 13:27:15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchases`
+--
+
+CREATE TABLE `purchases` (
+  `id_purchase` int(10) UNSIGNED NOT NULL,
+  `tgl_purchase` date NOT NULL,
+  `id_produk` int(10) UNSIGNED NOT NULL,
+  `qty_purchase` int(11) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `purchases`
+--
+
+INSERT INTO `purchases` (`id_purchase`, `tgl_purchase`, `id_produk`, `qty_purchase`, `status`, `created_at`, `updated_at`) VALUES
+(2, '2019-12-24', 2, 20, 1, '2019-12-24 13:14:09', '2019-12-24 13:16:00'),
+(3, '2019-12-24', 1, 10, 1, '2019-12-24 13:15:51', '2019-12-24 13:16:00'),
+(6, '2019-12-24', 2, 20, 1, '2019-12-24 13:31:50', '2019-12-24 13:32:05');
+
+--
+-- Triggers `purchases`
+--
+DELIMITER $$
+CREATE TRIGGER `barang_masuk` AFTER INSERT ON `purchases` FOR EACH ROW BEGIN
+	UPDATE products SET stok_produk = stok_produk+NEW.qty_purchase
+    WHERE id_produk = NEW.id_produk;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `cancel_purchase` AFTER DELETE ON `purchases` FOR EACH ROW BEGIN
+	UPDATE products SET stok_produk = products.stok_produk - OLD.qty_purchase
+	WHERE id_produk = OLD.id_produk;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sells`
+--
+
+CREATE TABLE `sells` (
+  `id_sell` int(10) UNSIGNED NOT NULL,
+  `tgl_sell` date NOT NULL,
+  `id_karyawan` int(10) UNSIGNED NOT NULL,
+  `id_produk` int(10) UNSIGNED NOT NULL,
+  `qty` int(11) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sells`
+--
+
+INSERT INTO `sells` (`id_sell`, `tgl_sell`, `id_karyawan`, `id_produk`, `qty`, `status`, `created_at`, `updated_at`) VALUES
+(1, '2019-12-23', 9, 1, 2, 1, '2019-12-23 13:27:42', '2019-12-23 13:28:09'),
+(2, '2019-12-23', 9, 2, 3, 1, '2019-12-23 13:27:59', '2019-12-23 13:28:09'),
+(3, '2019-12-23', 9, 2, 2, 1, '2019-12-23 16:26:29', '2019-12-23 16:26:55'),
+(6, '2019-12-23', 10, 2, 2, 1, '2019-12-23 16:34:41', '2019-12-23 16:34:50');
+
+--
+-- Triggers `sells`
+--
+DELIMITER $$
+CREATE TRIGGER `cancel_sell` AFTER DELETE ON `sells` FOR EACH ROW BEGIN
+	UPDATE products SET stok_produk = products.stok_produk + OLD.qty
+	WHERE id_produk = OLD.id_produk;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `pengambilan` AFTER INSERT ON `sells` FOR EACH ROW BEGIN
+	UPDATE products SET stok_produk = stok_produk-NEW.qty
+    WHERE id_produk = NEW.id_produk;
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -193,13 +282,13 @@ INSERT INTO `products` (`id_produk`, `kode_produk`, `nama_produk`, `id_kategori`
 --
 
 CREATE TABLE `suppliers` (
-  `id_supplier` int(10) NOT NULL,
+  `id_supplier` int(10) UNSIGNED NOT NULL,
   `nama_supplier` varchar(100) NOT NULL,
   `cp` varchar(100) NOT NULL,
   `alamat_supplier` text NOT NULL,
   `telp_supplier` varchar(20) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -217,10 +306,10 @@ INSERT INTO `suppliers` (`id_supplier`, `nama_supplier`, `cp`, `alamat_supplier`
 --
 
 CREATE TABLE `units` (
-  `id_unit` int(10) NOT NULL,
+  `id_unit` int(10) UNSIGNED NOT NULL,
   `nama_unit` varchar(100) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -255,8 +344,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `username`, `email`, `akses`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(4, 'Administrator', 'admin', 'admin@contoh.com', 'admin', '$2y$10$e/4AEFdynOYFe3GLoEZ7luQ6KSvZUAecX8hgE.oIzRzLi0BKr.u4C', 'v2Bq7Ih36zkMzb3PygdGiEwEz6wzI9PjhQmY4Zag7Km56V41nV2rFfTRkTZp', '2019-03-27 09:18:02', '2019-11-28 02:18:04'),
-(6, 'Arif Rusman', 'arif', 'arif@contoh.com', 'operator', '$2y$10$Ocijq6DGVBlbdawCnM.XFu7yIhr99MPeuDn.DICkqQbQpSITihkmG', NULL, '2019-08-26 15:54:31', '2019-08-26 15:54:31');
+(4, 'Administrator', 'admin', 'admin@contoh.com', 'admin', '$2y$10$e/4AEFdynOYFe3GLoEZ7luQ6KSvZUAecX8hgE.oIzRzLi0BKr.u4C', 'tn8WxclnEYX6yB6mOUzibwrN3H26xzoOb64vdDGrI3RIaoqD5JB3dshcDbYj', '2019-03-27 09:18:02', '2019-11-28 02:18:04'),
+(6, 'Arif Rusman', 'arif', 'arif@contoh.com', 'operator', '$2y$10$Ocijq6DGVBlbdawCnM.XFu7yIhr99MPeuDn.DICkqQbQpSITihkmG', NULL, '2019-08-26 15:54:31', '2019-08-26 15:54:31'),
+(7, 'Ade Eriviati', 'ade', 'ade@contoh.com', 'operator', '$2y$10$EZngxih2juu1z8oKHjhYUeEX.5dZmJfYWRFV.xZJMaI.2MOa2pd.W', 'cSByCzjxhCgddScqwyF06YZirziUTm4bgqSc6lYsLpytck5AotxETXLwy7OY', '2019-12-23 13:36:39', '2019-12-23 13:36:39');
 
 --
 -- Indexes for dumped tables
@@ -275,7 +365,8 @@ ALTER TABLE `agamas`
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id_kategori`);
+  ADD PRIMARY KEY (`id_kategori`),
+  ADD KEY `id_kategori` (`id_kategori`);
 
 --
 -- Indexes for table `employees`
@@ -310,19 +401,42 @@ ALTER TABLE `password_resets`
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id_produk`);
+  ADD PRIMARY KEY (`id_produk`),
+  ADD KEY `id_produk` (`id_produk`),
+  ADD KEY `id_kategori` (`id_kategori`),
+  ADD KEY `id_unit` (`id_unit`),
+  ADD KEY `id_supplier` (`id_supplier`);
+
+--
+-- Indexes for table `purchases`
+--
+ALTER TABLE `purchases`
+  ADD PRIMARY KEY (`id_purchase`),
+  ADD KEY `id_purchase` (`id_purchase`),
+  ADD KEY `id_produk` (`id_produk`);
+
+--
+-- Indexes for table `sells`
+--
+ALTER TABLE `sells`
+  ADD PRIMARY KEY (`id_sell`),
+  ADD KEY `id_sell` (`id_sell`),
+  ADD KEY `id_karyawan` (`id_karyawan`),
+  ADD KEY `id_produk` (`id_produk`);
 
 --
 -- Indexes for table `suppliers`
 --
 ALTER TABLE `suppliers`
-  ADD PRIMARY KEY (`id_supplier`);
+  ADD PRIMARY KEY (`id_supplier`),
+  ADD KEY `id_supplier` (`id_supplier`);
 
 --
 -- Indexes for table `units`
 --
 ALTER TABLE `units`
-  ADD PRIMARY KEY (`id_unit`);
+  ADD PRIMARY KEY (`id_unit`),
+  ADD KEY `id_unit` (`id_unit`);
 
 --
 -- Indexes for table `users`
@@ -345,7 +459,7 @@ ALTER TABLE `agamas`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id_kategori` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_kategori` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `employees`
@@ -369,19 +483,31 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id_produk` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_produk` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `purchases`
+--
+ALTER TABLE `purchases`
+  MODIFY `id_purchase` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `sells`
+--
+ALTER TABLE `sells`
+  MODIFY `id_sell` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `suppliers`
 --
 ALTER TABLE `suppliers`
-  MODIFY `id_supplier` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_supplier` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `units`
 --
 ALTER TABLE `units`
-  MODIFY `id_unit` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_unit` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -399,6 +525,27 @@ ALTER TABLE `users`
 ALTER TABLE `employees`
   ADD CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`id_agama`) REFERENCES `agamas` (`id_agama`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `employees_ibfk_2` FOREIGN KEY (`id_gender`) REFERENCES `genders` (`id_gender`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `categories` (`id_kategori`),
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`id_unit`) REFERENCES `units` (`id_unit`),
+  ADD CONSTRAINT `products_ibfk_3` FOREIGN KEY (`id_supplier`) REFERENCES `suppliers` (`id_supplier`);
+
+--
+-- Constraints for table `purchases`
+--
+ALTER TABLE `purchases`
+  ADD CONSTRAINT `purchases_ibfk_1` FOREIGN KEY (`id_produk`) REFERENCES `products` (`id_produk`);
+
+--
+-- Constraints for table `sells`
+--
+ALTER TABLE `sells`
+  ADD CONSTRAINT `sells_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `employees` (`id_karyawan`),
+  ADD CONSTRAINT `sells_ibfk_2` FOREIGN KEY (`id_produk`) REFERENCES `products` (`id_produk`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
